b4d71459cd2f35709710f0ab41c9021d786243f9
David SPORN <sporniket.studio@gmail.com>
2024-02-02 08:03:20 +0100

    Reorganize build.sh : move pathes/vars generation to the beginning of the script

--- build.sh
+++ build.sh
@@ -1,33 +1,15 @@
 #!/bin/sh -ex
+
+### TODO : what is the use of SOURCE_DATE_EPOCH ??
+export SOURCE_DATE_EPOCH=$(git log -1 --format=%ct)
+
 ###=###=###=###=###=###=###=###=###=###=###=###=###=###=###=###=###
-### SETUP vars
+### SETUP pathes and vars -- WASI
 ###=###=###=###=###=###=###=###=###=###=###=###=###=###=###=###=###
 VERSION_OF_WASI_SDK_MAJOR="19"
 VERSION_OF_WASI_SDK="${VERSION_OF_WASI_SDK_MAJOR}.0"
-VERSION_OF_GMP="6.3.0"
-
-###=###=###=###=###=###=###=###=###=###=###=###=###=###=###=###=###
-### Download required projects
-###=###=###=###=###=###=###=###=###=###=###=###=###=###=###=###=###
-export SOURCE_DATE_EPOCH=$(git log -1 --format=%ct)
-cat >tmp.pathes-yices2.mk <<END
-# ---<[YICES2 pathes]>---
-YICES2_SRC := `pwd`/yices2-src
-YICES2_SRC_HOME := \${YICES2_SRC}/src
-YICES2_BUILD := `pwd`/yices2-build
-END
-
-YICES2_SRC=yices2-src
-YICES2_SRC_HOME=`pwd`/${YICES2_SRC}/src
-YICES2_BUILD=yices2-build
-
-###=###=###=###=###=###=###=###=###=###=###=###=###=###=###=###=###
-### Download required projects
-###=###=###=###=###=###=###=###=###=###=###=###=###=###=###=###=###
-### WASI sdk select, check existence or fetch
 WASI_SDK="wasi-sdk-${VERSION_OF_WASI_SDK}"
-WASI_SDK_URL=https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-${VERSION_OF_WASI_SDK_MAJOR}/${WASI_SDK}-linux.tar.gz
-if ! [ -d ${WASI_SDK} ]; then curl -L ${WASI_SDK_URL} | tar xzf -; fi
+
 cat >tmp.pathes-wasi.mk <<END
 # ---<[WASI SDK pathes]>---
 WASI_SDK_HOME := `pwd`/${WASI_SDK}
@@ -35,6 +17,98 @@ WASI_SDK_PATH := \${WASI_SDK_HOME}/bin
 WASI_SYSROOT := \${WASI_SDK_HOME}/share/wasi-sysroot
 PATH := \${WASI_SDK_PATH}:\${PATH}
 END
+
+###=###=###=###=###=###=###=###=###=###=###=###=###=###=###=###=###
+### SETUP pathes and vars -- GETOPT
+###=###=###=###=###=###=###=###=###=###=###=###=###=###=###=###=###
+cat >tmp.pathes-getopt.mk <<END
+# ---<[GETOPT pathes]>---
+GETOPT_HOME := `pwd`/getopt
+GETOPT_SRC := \${GETOPT_HOME}/getopt_long.c
+GETOPT_OBJ := \${GETOPT_HOME}/getopt_long.o
+END
+
+cat >tmp.vars-getopt.mk <<END
+# ---<[GETOPT variables]>---
+# Requires : tmp.pathes-wasi.mk
+
+# -- apps
+CC := \${WASI_SDK_PATH}/clang 
+
+# -- flags
+CFLAGS := --sysroot \${WASI_SYSROOT} -c 
+END
+
+###=###=###=###=###=###=###=###=###=###=###=###=###=###=###=###=###
+### SETUP pathes and vars -- GMP
+###=###=###=###=###=###=###=###=###=###=###=###=###=###=###=###=###
+VERSION_OF_GMP="6.3.0"
+cat >tmp.pathes-gmp.mk <<END
+# ---<[GMP pathes]>---
+GMP_RELEASE := gmp-${VERSION_OF_GMP}
+GMP_BUILD_DIR := `pwd`/\${GMP_RELEASE}-build
+GMP_PREFIX_DIR := `pwd`/\${GMP_RELEASE}-prefix"
+END
+
+cat >tmp.vars-gmp.mk <<END
+# ---<[GMP variables]>---
+# Requires : tmp.pathes-wasi.mk
+
+# -- apps
+CC := \${WASI_SDK_PATH}/clang
+CC_FOR_BUILD := gcc
+
+# -- flags 
+TARGET := wasm32-unknown-wasi
+ARCH := \${TARGET}
+SYSROOT := \${WASI_SDK_HOME}/share/wasi-sysroot
+CFLAGS := --target=\${TARGET} -D_WASI_EMULATED_SIGNAL --sysroot=\${SYSROOT}
+LDFLAGS := -Wl,--strip-all --sysroot=\${SYSROOT}
+LIBS_BEGIN := -lwasi-emulated-process-clocks -lwasi-emulated-signal
+LIBS_END := --sysroot=\${SYSROOT}
+LIBS := \${LIBS_BEGIN} \${LIBS_END}
+PKG_CONFIG_SYSROOT_DIR := \${SYSROOT}"
+END
+
+###=###=###=###=###=###=###=###=###=###=###=###=###=###=###=###=###
+### SETUP vars -- YICES
+###=###=###=###=###=###=###=###=###=###=###=###=###=###=###=###=###
+YICES2_SRC=yices2-src
+YICES2_SRC_HOME=`pwd`/${YICES2_SRC}/src
+YICES2_BUILD=yices2-build
+
+cat >tmp.pathes-yices2.mk <<END
+# ---<[YICES2 pathes]>---
+YICES2_SRC := `pwd`/yices2-src
+YICES2_SRC_HOME := \${YICES2_SRC}/src
+YICES2_BUILD := `pwd`/yices2-build
+END
+
+cat >tmp.vars-yices2.mk <<END
+# ---<[YICES2 variables]>---
+# Requires : tmp.pathes-wasi.mk, tmp.pathes-gmp.mk, tmp.pathes-yices2.mk
+
+# -- apps
+CC := \${WASI_SDK_PATH}/clang
+CC_FOR_BUILD := gcc
+
+# -- flags 
+SYSROOT := \${WASI_SDK_HOME}/share/wasi-sysroot
+CFLAGS := --target=\${TARGET} -D_WASI_EMULATED_SIGNAL -D_WASI_EMULATED_PROCESS_CLOCKS --sysroot=\${SYSROOT}
+CPPFLAGS := -I\${GMP_PREFIX_DIR}/include -I\${YICES2_SRC_HOME} -I\${YICES2_SRC_HOME}/include
+LDFLAGS := -Wl,--strip-all -L\${GMP_PREFIX_DIR}/lib --sysroot=\${SYSROOT}
+LIBS_BEGIN := -lwasi-emulated-process-clocks -lwasi-emulated-signal
+LIBS_END :=  --sysroot=\${SYSROOT}
+LIBS := \${LIBS_BEGIN} -lgmp \${GETOPT_OBJ} \${LIBS_END}
+PKG_CONFIG_SYSROOT_DIR := \${SYSROOT}
+END
+
+###=###=###=###=###=###=###=###=###=###=###=###=###=###=###=###=###
+### Download required projects
+###=###=###=###=###=###=###=###=###=###=###=###=###=###=###=###=###
+### WASI sdk select, check existence or fetch
+WASI_SDK_URL=https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-${VERSION_OF_WASI_SDK_MAJOR}/${WASI_SDK}-linux.tar.gz
+if ! [ -d ${WASI_SDK} ]; then curl -L ${WASI_SDK_URL} | tar xzf -; fi
 
 WASI_SDK_HOME=`pwd`/${WASI_SDK}
 WASI_SDK_PATH="${WASI_SDK_HOME}/bin"
@@ -48,13 +122,6 @@ fi
 echo "clang will be '$(which clang)'"
 
 ### GNU MP library select source tarball, check existence or fetch
-cat >tmp.pathes-gmp.mk <<END
-# ---<[GMP pathes]>---
-GMP_RELEASE := gmp-${VERSION_OF_GMP}
-GMP_BUILD_DIR := `pwd`/\${GMP_RELEASE}-build
-GMP_PREFIX_DIR := `pwd`/\${GMP_RELEASE}-prefix"
-END
-
 GMP_RELEASE="gmp-${VERSION_OF_GMP}"
 GMP_RELEASE_TARBALL="${GMP_RELEASE}.tar"
 GMP_RELEASE_TARBALL_COMPRESSED="${GMP_RELEASE}.tar.gz"
@@ -99,26 +166,6 @@ export ARCH="${TARGET}"
 ### Build Gnu MP
 ###=###=###=###=###=###=###=###=###=###=###=###=###=###=###=###=###
 
-cat >tmp.vars-gmp.mk <<END
-# ---<[GMP variables]>---
-# Requires : tmp.pathes-wasi.mk
-
-# -- apps
-CC := \${WASI_SDK_PATH}/clang
-CC_FOR_BUILD := gcc
-
-# -- flags 
-TARGET := wasm32-unknown-wasi
-ARCH := \${TARGET}
-SYSROOT := \${WASI_SDK_HOME}/share/wasi-sysroot
-CFLAGS := --target=\${TARGET} -D_WASI_EMULATED_SIGNAL --sysroot=\${SYSROOT}
-LDFLAGS := -Wl,--strip-all --sysroot=\${SYSROOT}
-LIBS_BEGIN := -lwasi-emulated-process-clocks -lwasi-emulated-signal
-LIBS_END := --sysroot=\${SYSROOT}
-LIBS := \${LIBS_BEGIN} \${LIBS_END}
-PKG_CONFIG_SYSROOT_DIR := \${SYSROOT}"
-END
-
 export LIBS="${LIBS_BEGIN} ${LIBS_END}"
 if ! [ -d ${GMP_PREFIX_DIR} ]; then 
     mkdir "${GMP_PREFIX_DIR}"
@@ -142,48 +189,11 @@ fi
 ###=###=###=###=###=###=###=###=###=###=###=###=###=###=###=###=###
 ### Build getopt_long.o
 ###=###=###=###=###=###=###=###=###=###=###=###=###=###=###=###=###
-cat >tmp.pathes-getopt.mk <<END
-# ---<[GETOPT pathes]>---
-GETOPT_HOME := $(pwd)/getopt
-GETOPT_SRC := \${GETOPT_HOME}/getopt_long.c
-GETOPT_OBJ := \${GETOPT_HOME}/getopt_long.o
-END
-
-cat >tmp.vars-getopt.mk <<END
-# ---<[GETOPT variables]>---
-# Requires : tmp.pathes-wasi.mk
-
-# -- apps
-CC := \${WASI_SDK_PATH}/clang 
-
-# -- flags
-CFLAGS := --sysroot \${WASI_SYSROOT} -c 
-END
-
 make -C $(pwd)/getopt
 
 ###=###=###=###=###=###=###=###=###=###=###=###=###=###=###=###=###
 ### Build Yices2
 ###=###=###=###=###=###=###=###=###=###=###=###=###=###=###=###=###
-
-cat >tmp.vars-yices2.mk <<END
-# ---<[YICES2 variables]>---
-# Requires : tmp.pathes-wasi.mk, tmp.pathes-gmp.mk, tmp.pathes-yices2.mk
-
-# -- apps
-CC := \${WASI_SDK_PATH}/clang
-CC_FOR_BUILD := gcc
-
-# -- flags 
-SYSROOT := \${WASI_SDK_HOME}/share/wasi-sysroot
-CFLAGS := --target=\${TARGET} -D_WASI_EMULATED_SIGNAL -D_WASI_EMULATED_PROCESS_CLOCKS --sysroot=\${SYSROOT}
-CPPFLAGS := -I\${GMP_PREFIX_DIR}/include -I\${YICES2_SRC_HOME} -I\${YICES2_SRC_HOME}/include
-LDFLAGS := -Wl,--strip-all -L\${GMP_PREFIX_DIR}/lib --sysroot=\${SYSROOT}
-LIBS_BEGIN := -lwasi-emulated-process-clocks -lwasi-emulated-signal
-LIBS_END :=  --sysroot=\${SYSROOT}
-LIBS := \${LIBS_BEGIN} -lgmp \${GETOPT_OBJ} \${LIBS_END}
-PKG_CONFIG_SYSROOT_DIR := \${SYSROOT}
-END
 
 pwd
 export CFLAGS="--target=wasm32-unknown-wasi -D_WASI_EMULATED_SIGNAL -D_WASI_EMULATED_PROCESS_CLOCKS --sysroot=${SYSROOT}"
